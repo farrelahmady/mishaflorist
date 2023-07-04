257ab75a9147ea849d4d11ed6401a8993aba99cd
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2023-07-04 18:36:27 +0700

    app balde

--- database/migrations/2023_07_04_095555_create_products_table.php
+++ database/migrations/2023_07_04_095555_create_products_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('quantity')->default(0);
             $table->text('description')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
