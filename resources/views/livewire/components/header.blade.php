754f57485d0f520ac2100a279ac745c0c2dc0cbe
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2023-07-05 18:52:55 +0700

    header home adjustment

--- resources/views/livewire/components/header.blade.php
+++ resources/views/livewire/components/header.blade.php
@@ -18,7 +18,7 @@
                     class="block w-full h-1/6 rounded-full bg-stone-800 my-1 transition-all duration-300 ease-in-out after:block after:w-full after:h-full after:rounded-full after:bg-stone-800 after:translate-y-full after:transition-all after:duration-300 after:ease-in-out before:block before:w-full before:h-full before:rounded-full before:bg-stone-800 before:-translate-y-[200%] before:transition-all before:duration-300 before:ease-in-out"></span>
             </label>
             <label id="screen" for="hamburger-toggle"
-                class="hidden fixed inset-0 bg-white blur bg-opacity-25"></label>
+                class="hidden fixed inset-0 bg-black blur bg-opacity-25 -z-10"></label>
 
             <ul id="nav"
                 class="absolute invisible z-10 flex flex-col justify-start items-center w-1/2 h-screen bg-stone-500 top-full right-0 translate-x-full transition-all duration-300 ease-in-out">
