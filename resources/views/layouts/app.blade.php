b8d70eb6c86d8bd753d298e49ffaeb99aeea4ea1
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2023-07-10 14:39:13 +0700

    add livewire sweetalert component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,12 @@
     @yield('content')
 
     @livewireScripts
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script src="{{ asset('vendor/livewire-alert/livewire-alert.js') }}"></script>
+
+    <x-livewire-alert::flash />
 </body>
 
 </html>
